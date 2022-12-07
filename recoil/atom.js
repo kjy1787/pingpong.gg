1fc0fe7e7c9c7a73f5745c71a3a4730d3fe51606
ma9pie <ma9pie.dev@gmail.com>
2022-12-08 00:03:33 +0900

    배포 테스트

--- recoil/atom.js
+++ recoil/atom.js
@@ -1,27 +1,8 @@
 import { atom } from "recoil";
 
-export const countState = atom({
-  key: "countState",
-  default: 0,
-});
-
-export const todoListState = atom({
-  key: "todoListState",
-  default: [
-    { contents: "list 1", importance: "HIGH", status: "DONE" },
-    { contents: "list 2", importance: "LOW", status: "DOING" },
-    { contents: "list 3", importance: "MEDIUM", status: "DONE" },
-    { contents: "list 4", importance: "HIGH", status: "HOLD" },
-    { contents: "list 5", importance: "LOW", status: "DOING" },
-  ],
-});
-
-export const filterState = atom({
-  key: "filterState",
-  default: { importance: "", status: "" },
-});
+const timestamp = new Date().getTime();
 
 export const themeState = atom({
-  key: "themeState",
+  key: `themeState_${timestamp}`,
   default: "Dark",
 });
